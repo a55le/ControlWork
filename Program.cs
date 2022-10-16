23615306fb337c95acaaa8b4ccc7b34d4f08a449
Yuriy <yurashikov1@gmail.com>
2022-10-16 20:13:44 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿string MS = Console.ReadLine();
+int ValueSTR = new Random() .Next(1,4);
+int Lenght = MS.Length;
+string Fin = String.Empty;
+if (ValueSTR == 1)
+{
+int Number1 = new Random() .Next(1,Lenght);
+Fin = $"{MS[Number1]}";
+}
